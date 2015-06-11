51dd8b6c7676017d929dee77c5d54af34207a4f5
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-06-11 16:54:04 -0700

    Add DetailViewController and Navigation Controller.

---
+++ TableViewExercise/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  TableViewExercise
+//
+//  Created by Joao Paulo Galvao Alves on 6/11/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
